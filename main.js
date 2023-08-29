0ce1d8123514e4d31b740dfaa40a721c11d83682
orlando-ts <orlando.tijerina@me.com>
2023-08-29 16:17:51 -0600

    added 1 kata

--- main.js
+++ main.js
@@ -10,3 +10,13 @@
 function nameShuffler(str){
   return str.split(' ').reverse().join(' ')
  }
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+// Return True if you're better, else False!
+
+ function betterThanAverage(classPoints, yourPoints) {
+  const sum = classPoints.reduce((acc, cv) => acc + cv , 0);
+  return sum/classPoints.length < yourPoints;
+}
