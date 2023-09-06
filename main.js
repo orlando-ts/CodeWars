56bab992963341341c6a9218bf1dc2c9906687e3
orlando-ts <orlando.tijerina@me.com>
2023-09-05 18:55:11 -0600

    new 6 kata

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+// 6 kyu
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Example
+//"camelCasing"  =>  "camel Casing"
+
+function solution(string) {
+  return string.split(/(?=[A-Z])/).join(" ");
+}
+
 // 7 kyu
 
 // 8 kyu
-
 
 //Write a function that returns a string in which firstname is swapped with last name.
 //Example(Input --> Output)
@@ -11,12 +19,6 @@ function nameShuffler(str){
   return str.split(' ').reverse().join(' ')
  }
 
-// There was a test in your class and you passed it. Congratulations!
-// But you're an ambitious person. You want to know if you're better than the average student in your class.
-// You receive an array with your peers' test scores. Now calculate the average and compare your score!
-// Return True if you're better, else False!
-
- function betterThanAverage(classPoints, yourPoints) {
-  const sum = classPoints.reduce((acc, cv) => acc + cv , 0);
-  return sum/classPoints.length < yourPoints;
-}
+ function solution(string) {
+  return string.split(/(?=[A-Z])/).join(' ')
+ }
