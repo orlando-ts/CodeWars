2c48674411ed1a5c090fc9fb06a18acd99274a8b
Orlando Tijerina <orlando@192.168.1.39>
2023-09-07 13:00:21 -0600

    Added 6kyu kata :wq

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-// 6 kyu
+// // 6 kyu
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+  const individualWords = x.split(' ')
+  const numerizedWords = individualWords.map(e => e.split('').reduce((acc, c) => acc + c.charCodeAt(0) - 96, 0))
+  return individualWords[numerizedWords.indexOf(Math.max(...numerizedWords))]
+}
+
 //Complete the solution so that the function will break up camel casing, using a space between words.
 //Example
 //"camelCasing"  =>  "camel Casing"
