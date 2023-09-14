29236b505aef7d26bd0d178ca317fe60102140c1
Orlando Tijerina <orlando.tijerina@me.com>
2023-09-14 13:33:50 -0600

    wtf

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // // 6 kyu
 
-let cardinalCount = {}
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+// let cardinalCount = {}
 
 function countCardinals(arr){
   for(const ele of arr){
@@ -9,11 +12,12 @@ function countCardinals(arr){
     }else{
     cardinalCount[ele] = 1
     }}
-  return cardinalCount
 }
 
 function isValidWalk(walk) {
   countCardinals(walk)
+  return cardinalCount.n === cardinalCount.s && cardinalCount.e === cardinalCount.w && walk.length === 10
+  cardinalCount = {}
 }
 // Given a string of words, you need to find the highest scoring word.
 
